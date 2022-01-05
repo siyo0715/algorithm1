025fdd726641194f122b31efb23658efbedb67ad
向山　翔 <hms0175km@gmail.com>
2022-01-05 16:23:52 +0900

    [Add]最小値と最大値

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const a = [20, 10, 30, 60, 50, 90, 70, 40]
-        //[10, 20, 30, 40, 50, 60, 70, 90]
+        //[10, 20, 30, 40, 50, 60, 70, 90]; //8
 function bsort(array) {
     //配列の長さを取得
     let len = array.length;
@@ -11,6 +11,7 @@ function bsort(array) {
             }
         }
     }
-    return array;
+    // return array;
+    return {min:array[0],max:array[len-1]}
 }
 console.log(bsort(a));
